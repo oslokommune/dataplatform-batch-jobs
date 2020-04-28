94c3e2fafd9193c896930b1c63bc35ddc86bd65d
Simen Heggestøyl <simen.heggestoyl@byr.oslo.kommune.no>
2020-04-28 12:44:31 +0200

    DP-631 Dispatch on job name in startup script
    Make it possible to run different batch jobs by providing the `JOB_NAME`
    variable to the startup script.

--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-PYTHONPATH=. alembic upgrade head;
+case $JOB_NAME in
+    s3_access_log_aggregator)
+        PYTHONPATH=. alembic upgrade head;
+        luigid --background;
+        python -m luigi --module batch.s3_access_log_aggregator.tasks Run --days ${DAYS:-4} ;;
 
-luigid --background;
-python -m luigi --module batch.aggregator.tasks Run --days ${DAYS:-4};
+    s3_dataset_scanner)
+        luigid --background;
+        python -m luigi --module batch.s3_dataset_scanner.tasks ExtractDatasetsFromS3Metadata --date $(date -I) ;;
+
+    "")
+        echo "Please set JOB_NAME" ;;
+
+    *)
+        echo "Unknown job: \"$JOB_NAME\"" ;;
+esac
