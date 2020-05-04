a29cd4d3103393014287b491fa91d06e34de5f2d
Simen Heggestøyl <simen.heggestoyl@byr.oslo.kommune.no>
2020-05-04 13:20:14 +0200

    Add helper for converting CSV to Parquet streams
    Add a helper function for converting CSV files to Parquet data byte
    streams.

--- tests/s3_dataset_scanner/test_extract_datasets_from_s3_metadata.py
+++ tests/s3_dataset_scanner/test_extract_datasets_from_s3_metadata.py
@@ -1,23 +1,14 @@
 from datetime import date
-from io import BytesIO
-from unittest.mock import Mock
-
-import pandas as pd
-from fastparquet import write as pq_write
 
 from batch.models import DatasetOnDate
 from batch.s3_dataset_scanner.extract_datasets_from_s3_metadata import (
     extract_datasets_from_s3_metadata,
 )
+from tests.util import csv_file_to_parquet_source
 
 
 def test_extract_datasets_from_s3_metadata(test_db_session):
-    input_df = pd.read_csv("tests/s3_dataset_scanner/data/raw.csv")
-    input_data = BytesIO()
-    input_data.close = Mock()
-    pq_write(input_data, input_df, open_with=lambda io, mode: io)
-    input_data.seek(0)
-    input_source = Mock(path=input_data)
+    input_source = csv_file_to_parquet_source("tests/s3_dataset_scanner/data/raw.csv")
 
     extract_datasets_from_s3_metadata(input_source, date(2020, 1, 1))
 
--- tests/util.py
+++ tests/util.py
@@ -1,6 +1,9 @@
 from io import BytesIO, StringIO
 from unittest.mock import Mock
 
+import pandas as pd
+from fastparquet import write as pq_write
+
 
 def _mock_output_target(result_class):
     result = result_class()
@@ -24,3 +27,16 @@ def mock_string_output_target():
     read and write to and from the stream.
     """
     return _mock_output_target(StringIO)
+
+
+def csv_file_to_parquet_source(filename):
+    """Return a mock object with a `path` to a readable byte stream of Parquet
+    data, converted from the CSV file `filename`.
+    """
+    input_df = pd.read_csv(filename)
+    input_data = BytesIO()
+    input_data.close = Mock()
+    pq_write(input_data, input_df, open_with=lambda io, mode: io)
+    input_data.seek(0)
+
+    return Mock(path=input_data)
