8e67f675fac07763121e7d29f1718fa46208e628
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2022-02-09 12:38:13 +0100

    Ensure closing of test files
    Issue discovered by CodeQL.

--- tests/s3_access_log_aggregator/test_tasks.py
+++ tests/s3_access_log_aggregator/test_tasks.py
@@ -35,10 +35,11 @@ def test_s3_logs_to_csv():
     s3 = boto3.resource("s3", region_name="us-east-1")
     s3.create_bucket(Bucket="test-input-bucket")
     s3.create_bucket(Bucket="test-output-bucket")
-    s3.Object(
-        "test-input-bucket",
-        "logs/s3/test-output-bucket/2020-02-13-11-43-07-27B0F6A55F241BF8",
-    ).put(Body=open("tests/s3_access_log_aggregator/data/s3_access_log.txt", "rb"))
+    with open("tests/s3_access_log_aggregator/data/s3_access_log.txt", "rb") as f:
+        s3.Object(
+            "test-input-bucket",
+            "logs/s3/test-output-bucket/2020-02-13-11-43-07-27B0F6A55F241BF8",
+        ).put(Body=f)
 
     luigi.build([S3LogsToCSV("2020-02-13-11", "test")], local_scheduler=True)
 
@@ -56,10 +57,11 @@ def test_s3_logs_to_csv_in_the_future():
     s3 = boto3.resource("s3", region_name="us-east-1")
     s3.create_bucket(Bucket="test-input-bucket")
     s3.create_bucket(Bucket="test-output-bucket")
-    s3.Object(
-        "test-input-bucket",
-        "logs/s3/test-output-bucket/2020-02-13-11-43-07-27B0F6A55F241BF8",
-    ).put(Body=open("tests/s3_access_log_aggregator/data/s3_access_log.txt", "rb"))
+    with open("tests/s3_access_log_aggregator/data/s3_access_log.txt", "rb") as f:
+        s3.Object(
+            "test-input-bucket",
+            "logs/s3/test-output-bucket/2020-02-13-11-43-07-27B0F6A55F241BF8",
+        ).put(Body=f)
 
     # Pretend we're in the past relative to the passed timestamp.
     batch.s3_access_log_aggregator.tasks.datetime.utcnow.return_value = (
@@ -77,10 +79,11 @@ def test_s3_logs_to_csv_in_the_future():
 def test_enrich_csv_to_parquet():
     s3 = boto3.resource("s3", region_name="us-east-1")
     s3.create_bucket(Bucket="test-output-bucket")
-    s3.Object(
-        "test-output-bucket",
-        "test/raw/red/dataplatform/dataplatform-s3-logs/version=1/year=2020/month=2/day=13/hour=11/data.csv",
-    ).put(Body=open("tests/s3_access_log_aggregator/data/raw.csv", "rb"))
+    with open("tests/s3_access_log_aggregator/data/raw.csv", "rb") as f:
+        s3.Object(
+            "test-output-bucket",
+            "test/raw/red/dataplatform/dataplatform-s3-logs/version=1/year=2020/month=2/day=13/hour=11/data.csv",
+        ).put(Body=f)
 
     EnrichCSVToParquet("2020-02-13-11", "test").run()
 
@@ -95,10 +98,11 @@ def test_aggregate_to_db(test_db_session):
     s3 = boto3.resource("s3", region_name="us-east-1")
     s3.create_bucket(Bucket="test-output-bucket")
     for hour in range(0, 24):
-        s3.Object(
-            "test-output-bucket",
-            f"test/raw/red/dataplatform/dataplatform-s3-logs/version=1/year=2020/month=2/day=13/hour={hour}/data.csv",
-        ).put(Body=open("tests/s3_access_log_aggregator/data/raw.csv", "rb"))
+        with open("tests/s3_access_log_aggregator/data/raw.csv", "rb") as f:
+            s3.Object(
+                "test-output-bucket",
+                f"test/raw/red/dataplatform/dataplatform-s3-logs/version=1/year=2020/month=2/day=13/hour={hour}/data.csv",
+            ).put(Body=f)
 
     luigi.build([AggregateToDB("2020-02-13", "test")], local_scheduler=True)
 
